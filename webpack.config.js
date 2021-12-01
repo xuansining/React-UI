c5869dd5a1d6c0be08c078d9056fb541943157f3
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-12-01 13:08:30 +0800

    webpack可以运行了

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path=require('path')
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'MUI',
+        libraryTarget:'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader:'awesome-typescript-loader'
+            }
+        ]
+    }
+}
+
+
