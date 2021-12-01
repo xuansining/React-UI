1c1eaed678066649b4c69ffe7ddb71cc7f88a33c
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-12-01 22:00:31 +0800

    在Webpack引入scss

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import './icon.scss'
 interface IconProp{
     name?: string
 }
 const Icon:React.FC<IconProp> = (props) => {
     const name=props.name;
     return (
-        <span>
-            <svg>
+
+            <svg className={'mina-icon'}>
                 <use xlinkHref={`#${name}`}/>
             </svg>
-        </span>
+
     );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,12 @@ module.exports = {
                 test: /\.tsx?$/,
                 loader: 'awesome-typescript-loader'
 
-            },{
+            }, {
                 test: /\.svg$/,
                 loader: 'svg-sprite-loader',
+            }, {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
