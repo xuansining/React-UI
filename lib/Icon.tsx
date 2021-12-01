f45da14ffd5674d96c951382d03e38c7944c8709
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-12-01 21:14:46 +0800

    初步引入svg Icon

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 interface IconProp{
     name?: string
 }
-const Icon:React.FC<IconProp> = () => {
+const Icon:React.FC<IconProp> = (props) => {
+    const name=props.name;
     return (
         <span>
-            Icon
+            <svg>
+                <use xlinkHref={`#${name}`}/>
+            </svg>
         </span>
     );
 };
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,6 +3,14 @@ import ReactDom from 'react-dom'
 import Icon from "./Icon";
 
 
+import wechat from './icons/wechat.svg'
+import qq from './icons/qq.svg'
+import  alipay from './icons/alipay.svg'
 
 
-ReactDom.render(<div><Icon name={'hello'}/></div>, document.getElementById('root'))
+ReactDom.render(<div>
+    <Icon name={wechat.id}/>
+   <Icon name={qq.id}/>
+   <Icon name={alipay.id}/>
+
+</div>, document.getElementById('root'))
