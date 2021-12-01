b1901d6398e027127cc88dcef01b23551447ded2
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-12-01 21:23:58 +0800

    importAll Icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,15 +2,11 @@ import * as React from "react";
 import ReactDom from 'react-dom'
 import Icon from "./Icon";
 
-
-import wechat from './icons/wechat.svg'
-import qq from './icons/qq.svg'
-import  alipay from './icons/alipay.svg'
-
+import './importAll'
 
 ReactDom.render(<div>
-    <Icon name={wechat.id}/>
-   <Icon name={qq.id}/>
-   <Icon name={alipay.id}/>
+    <Icon name={'wechat'}/>
+   <Icon name={'qq'}/>
+   <Icon name={'alipay'}/>
 
 </div>, document.getElementById('root'))
